ef79de310f213da0a4abe03540b88cda4a781ec2
marianoponte <mariano.ponte@hotmail.com>
2021-02-24 18:46:01 -0300

    prueba de cambio host en servidor

--- inspire-backend/app/app.py
+++ inspire-backend/app/app.py
@@ -42,4 +42,4 @@ if config_decouple('PRODUCTION', default=False):
 app = create_app(environment)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0',port=5000)
--- inspire-frontend/src/components/products/Producto.js
+++ inspire-frontend/src/components/products/Producto.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Canje from '../canje/Canje';
 import DeleteProduct from './DeleteProduct';
+import jwt_decode from 'jwt-decode';
 
 const Producto = (props) => {
 
     const { id, nombre, urlImage, puntos, descripcion } = props.producto
+
+    const [isAdmin, setIsAdmin] = useState(false)
+
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+        var token = localStorage.getItem('token')
+        var memberLogged = jwt_decode(token);
+         if (memberLogged.permiso == "Admin") {
+            setIsAdmin(true)
+        } }
+    }, []);
+
     return (
         <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
             <div className="card">
@@ -17,8 +30,8 @@ const Producto = (props) => {
                         productId={id}
                         puntos={puntos} />
                         <p></p>
-                    <DeleteProduct 
-                        productId={id}/>
+                    { isAdmin && <DeleteProduct 
+                        productId={id}/>}
                 </div>
             </div>
         </div>
